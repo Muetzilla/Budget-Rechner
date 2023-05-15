ad8899848cb846a1f4b4f11ee65d251102b78e06
Muetzilla <muetz-github@gasbichler.ch>
2023-05-15 08:43:45 +0200

    Einnahmen & Ausgaben

---
+++ js/budgetrechner.js
@@ -0,0 +1,13 @@
+function handleRevenue(){
+    let revenueInputElement = document.getElementById("revenueInput");
+    let revenue = revenueInputElement.value;
+
+    console.log("Die Einnahmen betragen: " + revenue);
+}
+
+function handleExpanses(){
+    let expensesInputElement = document.getElementById("expensesInput");
+    let expenses = expensesInputElement.value;
+
+    console.log("Die Ausgaben betragen: " + expenses);
+}
