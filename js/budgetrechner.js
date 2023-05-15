8eb98534281632862fc0f9cb0d3b350c416805ae
Muetzilla <muetz-github@gasbichler.ch>
2023-05-15 20:00:11 +0200

    Add balance

--- js/budgetrechner.js
+++ js/budgetrechner.js
@@ -1,7 +1,8 @@
 function handleRevenue(){
     let revenueInputElement = document.getElementById("revenueInput");
     let revenue = revenueInputElement.value;
-
+    let revenueTotalField = document.getElementById("totalrevenuePlaceholder");
+    revenueTotalField.innerHTML = revenue;
     console.log("Die Einnahmen betragen: " + revenue);
 }
 
@@ -9,5 +10,7 @@ function handleExpanses(){
     let expensesInputElement = document.getElementById("expensesInput");
     let expenses = expensesInputElement.value;
 
+    let expansesTotalField = document.getElementById("totalexpensePlaceholder");
+    expansesTotalField.innerHTML = expenses;
     console.log("Die Ausgaben betragen: " + expenses);
 }
