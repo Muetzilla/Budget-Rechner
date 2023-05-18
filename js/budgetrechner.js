05fa8ae4b34f6ceb99e8ad744f95c2673e93c947
Muetzilla <muetz-github@gasbichler.ch>
2023-05-18 18:51:23 +0200

    Calculating Budget with JS

--- js/budgetrechner.js
+++ js/budgetrechner.js
@@ -1,16 +1,47 @@
+var totalBudget = 0;
+var totalRevenue = 0;
+var totalExpense = 0;
+
 function handleRevenue(){
     let revenueInputElement = document.getElementById("revenueInput");
     let revenue = revenueInputElement.value;
-    let revenueTotalField = document.getElementById("totalrevenuePlaceholder");
-    revenueTotalField.innerHTML = revenue;
-    console.log("Die Einnahmen betragen: " + revenue);
+    calculateRevenue(parseFloat(revenue));
+
+    let revenueFormElement = document.getElementById("addRevenueForm");
+    revenueFormElement.reset();
 }
 
-function handleExpanses(){
+function handleExpenses(){
     let expensesInputElement = document.getElementById("expensesInput");
     let expenses = expensesInputElement.value;
+    calculateExpense(parseFloat(expenses));
+
+    let expensesFormElement = document.getElementById("addExpensesForm");
+    expensesFormElement.reset();
+
+}
+
+function calculateRevenue(revenueAmount){
+    totalRevenue += revenueAmount;
+
+    let revenueTotalField = document.getElementById("totalrevenuePlaceholder");
+    revenueTotalField.innerHTML = totalRevenue;
+    calculateBudget();
+    console.log(totalRevenue);
+}
+
+function calculateExpense(expensesAmount){
+    totalExpense += expensesAmount;
 
     let expansesTotalField = document.getElementById("totalexpensePlaceholder");
-    expansesTotalField.innerHTML = expenses;
-    console.log("Die Ausgaben betragen: " + expenses);
+    expansesTotalField.innerHTML = totalExpense;
+
+    calculateBudget();
+}
+
+function calculateBudget(){
+    totalBudget = totalRevenue - totalExpense;
+
+    let budgetField = document.getElementById("budgetPlaceholder");
+    budgetField.innerHTML = totalBudget;
 }
